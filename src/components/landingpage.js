0e7b59b07b13efe46e277ccc256317bbe0cc5dec
asharomu <25918532+asharomu@users.noreply.github.com>
2020-04-22 09:53:17 -0400

    Added rubberband animation to landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,54 +1,58 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-import Zoom from 'react-reveal/Zoom';
+import RubberBand from 'react-reveal/RubberBand';
+import Bounce from 'react-reveal/Bounce';
+
 
 
 class Landing extends Component {
   render() {
     return(
       <div style={{width: '100%', margin: 'auto'}}>
+       
         <Grid className="landing-grid">
-          <Cell col={12}>
-            <Zoom>
+            <Cell col={12}>
+            <RubberBand>
               <img
                 margin="auto"
                 src="https://cdn0.iconfinder.com/data/icons/transportation-vehicles-5/24/transportation_transport_vehicles_mustang_car-512.png"
                 alt="avatar"
                 className="avatar-img"
                 />
-            </Zoom>
-      
-            <div className="banner-text">
-              <h1>Alejandro's Portfolio</h1>
-            <hr/>
+            </RubberBand>
+        <Bounce left>
+              <div className="banner-text">
+                <h1>Alejandro's Portfolio</h1>
+              <hr/>
 
-          <p>Python | C/C++ | Java | React  | NodeJS | Java | ROS</p>
+            <p>Python | C/C++ | Java | React  | NodeJS | Java | ROS</p>
 
-        <div className="social-links">
+          <div className="social-links">
 
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
+            {/* LinkedIn */}
+            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-linkedin-square" aria-hidden="true" />
+            </a>
 
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square" aria-hidden="true" />
-          </a>
+            {/* Github */}
+            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-github-square" aria-hidden="true" />
+            </a>
 
-          {/* Freecodecamp */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-free-code-camp" aria-hidden="true" />
-          </a>
+            {/* Freecodecamp */}
+            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-free-code-camp" aria-hidden="true" />
+            </a>
 
-          {/* Youtube */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-youtube-square" aria-hidden="true" />
-          </a>
+            {/* Youtube */}
+            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-youtube-square" aria-hidden="true" />
+            </a>
 
-        </div>
-            </div>
-          </Cell>
+          </div>
+              </div>
+          </Bounce>
+            </Cell>
         </Grid>
       </div>
     )
