fc9e3f4cda749d726a37611eab509e257495d9d4
asharomu <25918532+asharomu@users.noreply.github.com>
2020-04-23 00:45:42 -0400

    Fixed education layout

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ class App extends Component {
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Home</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
+                <Link to="/aboutme"> Blog</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Portfolio</Link>}>
+        <Drawer title='Portfolio'>
             <Navigation>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
+              <Link to="/resume" style={{color: 'black'}}>Resume</Link>
+              <Link to="/aboutme" style={{color: 'black'}}>Blog</Link>
+              <Link to="/projects" style={{color: 'black'}}>Projects</Link>
+              <Link to="/contact" style={{color: 'black'}}>Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
--- src/components/education.js
+++ src/components/education.js
@@ -6,11 +6,11 @@ class Education extends Component {
     return(
       <Grid>
         <Cell col={4}>
-          <p>{this.props.startYear} - {this.props.endYear}</p>
           <img src={this.props.universityLogo} height={this.props.logoHeight}/>
         </Cell>
         <Cell col={8}>
           <h4 style={{marginTop:'0px'}}>{this.props.schoolName}</h4>
+          <p>{this.props.startYear} - {this.props.endYear}</p>
           <p>{this.props.schoolDescription}</p>
         </Cell>
       </Grid>
